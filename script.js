b13c4e69ad6467d370e3721aa4593f0ccd14d240
okysky1121 <okysky1121@gmail.com>
2020-10-16 10:06:23 +0900

    vw

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.addEventListener("load", function (_) {
   const pages = Array.from(document.querySelectorAll("#slider > .page"));
-  const x = getComputedStyle(pages[0]).width;
   let now = 0;
   const [left, right] = [
     document.querySelector("#left"),
     document.querySelector("#right"),
   ];
-  left.addEventListener("click", function (ev) {
+  left.addEventListener("click", function (_ev) {
     const page = pages[--now < 0 ? ++now : now];
-    page.style.setProperty("width", x);
+    page.style.setProperty("width", "100vw");
   });
-  right.addEventListener("click", function (ev) {
+  right.addEventListener("click", function (_ev) {
     const page = pages[now++];
     if (now === pages.length) return now--;
     page.style.setProperty("width", "0px");
