a7c4c985aa527fe6c2378e559f1954bf81b494ef
okysky1121 <okysky1121@gmail.com>
2020-10-16 10:19:49 +0900

    auto scroll

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 window.addEventListener("load", function (_) {
+  function sleep(ms) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(resolve, ms);
+    });
+  }
   const pages = Array.from(document.querySelectorAll("#slider > .page"));
   let now = 0;
   const [left, right] = [
@@ -14,4 +19,12 @@ window.addEventListener("load", function (_) {
     if (now === pages.length) return now--;
     page.style.setProperty("width", "0px");
   });
+  setInterval(async function () {
+    if (now === pages.length - 1)
+      for (let i = 0; i < 6; i++) {
+        await sleep(333);
+        left.click();
+      }
+    else right.click();
+  }, 3000);
 });
